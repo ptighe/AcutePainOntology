f44cec9409d1611a3fa5e606b3c43da935b00dfd
Patrick Tighe <ptighe@ufl.edu>
2016-02-05 18:03:51 -0500

    Think I got the tree.xpath to work, still need to test output to dfsimple

--- xml_testing/morexmlobjectify.py
+++ xml_testing/morexmlobjectify.py
@@ -1,16 +1,52 @@
 from lxml import objectify
 import pandas as pd
 
-path='pubmed_result_tighe.xml'
-xml = objectify.parse(open(path))
-root = xml.getroot()
+path='xml_testing/pubmed_result_tighe.xml'
+tree = objectify.parse(open(path))
+root = tree.getroot()
 parents = root.getchildren()
 
 parents
 
-for elem in xml.iter(tag='AbstractText'):
+for elem in tree.iter(tag='AbstractText'):
     print (elem.tag, elem.attrib, elem.text)
+
+for elem in tree.iterfind('//Abstract/AbstractText'):
+    print (elem.tag,elem.text)
+
+
+for elem in tree.iterfind('//PMID'):
+    print (elem.tag,elem.text)
+
+for elem in tree.iterfind('//Article/ArticleTitle'):
+    print (elem.tag,elem.text)
+
+# root.getchildren()[0].getchildren()
+#
+# for child in root.getchildren()[0].getiterator():
+#     print (child.tag)
+#
+# for child in root.getchildren()[0].getchildren()[1].getchildren():
+#     print (child.tag)
+
+pmid = tree.xpath('//PMID')[1]
+
+for i in range(0,3):
+    print (tree.xpath('//PMID')[i])
+
 
 
 df = pd.DataFrame(columns=('PMID','PubDateYear','PubDateMonth','JournalTitle',
                            'JournalAbbrev','ArticleTitle','AbstractText'))
+
+dfsimple = pd.DataFrame(columns=('PMID','ArticleTitle','AbstractText'))
+
+for i in range(len(parents)):
+    dfsimple.loc[i,'PMID']= tree.xpath('//PMID')[i]
+    dfsimple.loc[i,'ArticleTitle']=tree.xpath('//Article/ArticleTitle')[i]
+    dfsimple.loc[i,'AbstractText']=tree.xpath('//Abstract/AbstractText')[i]
+
+dfsimple
+    # pm_id = tree.xpath('//PMID')[i]
+    # article_title = tree.xpath('//Article/ArticleTitle')[i]
+    # abstract_text = tree.xpath('//Abstract/AbstractText')[i]
